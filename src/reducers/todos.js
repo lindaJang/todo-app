18ef12574b91ca355939864998e25e162f00392e
lindaJang <linda@plip.ai>
2017-05-16 16:48:41 +0900

    reducer : divide todos + todo

--- src/reducers/todos.js
+++ src/reducers/todos.js
@@ -1,26 +1,37 @@
+
+const todo = (state, action) =>{
+    switch (action.type){
+        case 'ADD_TODO':
+            return  {
+                id: action.id,
+                text: action.text,
+                completed: false
+            };
+        case 'TOGGLE_TODO':
+            if(state.id !== action.id)
+                return state;
+
+            return {
+                id: state.id,
+                text: state.text,
+                completed: !state.completed
+            };
+        default: return state
+    }
+}
+
 const todos = (state = [], action) => {
     switch (action.type) {
         case 'ADD_TODO':
             return [...state,
-                {
-                    id: action.id,
-                    text: action.text,
-                    completed: false
-                }];
+                todo(state,action)
+            ];
         case 'TOGGLE_TODO':
-            return state.map( todo => {
-                if(todo.id !== action.id)
-                    return todo;
-
-                return {
-                    id: todo.id,
-                    text: todo.text,
-                    completed: !todo.completed
-                };
-            });
+            return state.map( t => todo(t,action));
         default:
             return state;
     }
 };
+//TODO: ...todo 연산 안됨..
 
 export default todos;

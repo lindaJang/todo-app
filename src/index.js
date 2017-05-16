12ca87719a6937ce6ee1c2f8ecd079bf70e515e9
lindaJang <wannagenius@naver.com>
2017-05-17 07:48:49 +0900

    set visibility 기능 구현

--- src/index.js
+++ src/index.js
@@ -1,16 +1,67 @@
 import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 import {createStore} from 'redux';
-import {Provider} from 'react-redux';
+//import {Provider} from 'react-redux';
 import todoApp from './reducers/index';
 
 
 const store = createStore(todoApp);
 
+const FilterLink = ({
+    filter,
+    currentFilter,
+    children
+}) => {
+    if(filter === currentFilter){
+        return <span>{children}</span>;
+    }
+    return (
+       <a href='#'
+        onClick={e => {
+            e.preventDefault();
+            /*  a 태그의 경우, onclick 이벤트후 기본동작인 href 이벤트가 호출이된다.
+                그래서 onclick과 href가 둘다 호출되어 버리는 문제점이 발생한다.
+                이 때, onclick에서 preventDefault를 호출하게 되면, 다음 이벤트는 모두 무시가 되기 때문에 의도한대로 동작하게 된다. */
+            store.dispatch({
+                type:'SET_VISIBILITY_FILTER',
+                filter
+            });
+        }}
+       >
+           {children}
+       </a>
+    );
+};
+
+const getVisibilityTodos = (
+    todos,
+    filter
+) => {
+    switch(filter){
+        case 'SHOW_ALL':
+            return todos;
+        case 'SHOW_ACTIVE':
+            return todos.filter(
+                t => !t.completed
+            );
+        case 'SHOW_COMPLETE':
+            return todos.filter(
+                t => t.completed
+            );
+    }
+};
 
 let nextId = 0;
 class TodoApp extends Component {
     render() {
+        const {
+            todos,
+            visibilityFilter
+        } = this.props;
+        const visibleTodos = getVisibilityTodos(
+          todos,
+          visibilityFilter
+        );
         return (
             <div className="App">
                 <input ref={node =>{
@@ -25,7 +76,7 @@ class TodoApp extends Component {
 
                     this.input.value = '';
                 }}>Add todo</button>
-                <ul>{this.props.todos.map(todo =>
+                <ul>{visibleTodos.map(todo =>
                     <li key={todo.id}
                         onClick={ () => {store.dispatch({
                             type: 'TOGGLE_TODO',
@@ -39,6 +90,30 @@ class TodoApp extends Component {
                         {todo.text}</li>
                 )}
                 </ul>
+                <p>
+                    Show:
+                    {' '}
+                    <FilterLink
+                        filter='SHOW_ALL'
+                        currentFilter={visibilityFilter}
+                    >
+                        All
+                    </FilterLink>
+                    {', '}
+                    <FilterLink
+                        filter='SHOW_ACTIVE'
+                        currentFilter={visibilityFilter}
+                    >
+                        Active
+                    </FilterLink>
+                    {', '}
+                    <FilterLink
+                        filter='SHOW_COMPLETE'
+                        currentFilter={visibilityFilter}
+                    >
+                        Completed
+                    </FilterLink>
+                </p>
             </div>
         );
     }
@@ -46,11 +121,11 @@ class TodoApp extends Component {
 
 const render = () => {
     ReactDOM.render(
-        <Provider store={store}>
+       // <Provider store={store}>
             <TodoApp
-                todos={store.getState().todos}
-            />
-        </Provider>,
+                {...store.getState()}
+            />,
+       // </Provider>,
         document.getElementById('root')
     );
 };
--- src/reducers/__tests__/todos.test.js
+++ src/reducers/__tests__/todos.test.js
@@ -16,3 +16,35 @@ test('addTodo test', () => {
 
     expect(todos(prevState, action)).toEqual(nextState);
 });
+
+test('toggleTodo test', () => {
+    const prevState = [
+        {
+            id: 0,
+            text: 'run today',
+            completed: false
+        },
+        {
+            id: 1,
+            text: 'walk today',
+            completed: false
+        }
+    ];
+    const action = {
+        type: 'TOGGLE_TODO',
+        id: 0
+    };
+    const nextState = [
+        {
+            id: 0,
+            text: 'run today',
+            completed: true
+        },
+        {
+            id: 1,
+            text: 'walk today',
+            completed: false
+        }
+    ];
+    expect(todos(prevState,action)).toEqual(nextState);
+});

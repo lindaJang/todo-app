1cc24e79f87c6d04851a07d8abcbba262647d2fb
lindaJang <wannagenius@naver.com>
2017-05-17 23:57:17 +0900

    extracting Container components(FilterLink)

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 import {createStore} from 'redux';
 //import {Provider} from 'react-redux';
 import todoApp from './reducers/index';
 import TodoList from './components/TodoList';
 import AddTodo from './components/AddTodo';
-import Footer from './components/Footer';
+//import Footer from './components/Footer';
 
 const store = createStore(todoApp);
 
@@ -28,6 +28,83 @@ const getVisibilityTodos = (
             return
     }
 };
+const Link = ({
+                  active,
+                  children,
+                  onClick
+              }) => {
+    if(active){
+        return <span>{children}</span>;
+    }
+    return (
+        <a href='#'
+           onClick={e => {
+               e.preventDefault();
+               /*  a 태그의 경우, onclick 이벤트후 기본동작인 href 이벤트가 호출이된다.
+                그래서 onclick과 href가 둘다 호출되어 버리는 문제점이 발생한다.
+                이 때, onclick에서 preventDefault를 호출하게 되면, 다음 이벤트는 모두 무시가 되기 때문에 의도한대로 동작하게 된다. */
+               onClick();
+           }}
+        >
+            {children}
+        </a>
+    );
+};
+
+class FilterLink extends Component {
+    componentWillMount(){
+        this.subscribe = store.subscribe(() =>
+            this.forceUpdate()
+        );
+    }
+    componentWillUnmount(){
+        this.unsubscribe();
+    }
+    render(){
+        const props = this.props;
+        const state = store.getState();
+        return(
+            <Link
+                active={
+                    props.filter === state.visibilityFilter
+                }
+                onClick={() =>
+                    store.dispatch({
+                        type: 'SET_VISIBILITY_FILTER',
+                        filter: props.filter
+                    })
+                }
+            >
+                {props.children}
+            </Link>
+        );
+    }
+}
+
+const Footer = () => (
+    <p>
+        Show:
+        {' '}
+        <FilterLink
+            filter='SHOW_ALL'
+        >
+            All
+        </FilterLink>
+        {', '}
+        <FilterLink
+            filter='SHOW_ACTIVE'
+        >
+            Active
+        </FilterLink>
+        {', '}
+        <FilterLink
+            filter='SHOW_COMPLETE'
+        >
+            Completed
+        </FilterLink>
+    </p>
+);
+
 
 let nextId = 0;
 const TodoApp = ({
@@ -58,15 +135,7 @@ const TodoApp = ({
                 })
             }
         />
-        <Footer
-            visibilityFilter={visibilityFilter}
-            onFilterClick={filter =>
-            store.dispatch({
-                type: 'SET_VISIBILITY_FILTER',
-                filter
-            })
-            }
-        />
+        <Footer />
 
     </div>
 );
